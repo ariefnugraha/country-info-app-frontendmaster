a186007aa7ee4cb1661c63fb9af1dd9d27c490a8
Arief <muhammadariefnugraha77@gmail.com>
2021-08-11 20:33:25 +0700

    make detail page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import "./scss/style.scss";
 import Homepage from './pages/Homepage';
+import Detail from './pages/Detail';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Homepage} /></Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/detail" component={Detail} />
+      </Switch>
     </Router>
   );
 }
