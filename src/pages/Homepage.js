0d00ae7f9d645b11c2bbe8c92f5a1658d56b9ea2
Arief <muhammadariefnugraha77@gmail.com>
2021-08-11 19:51:13 +0700

    make card component for homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,12 +2,23 @@ import React from 'react'
 
 import Navbar from '../components/Navbar';
 import SearchFilter from '../components/SearchFilter';
+import CardItem from '../components/CardItem';
 
 export default function Homepage() {
     return (
         <div className="homepage day">
             <Navbar />
             <SearchFilter />
+            <div className="list-countries">
+                <CardItem />
+                <CardItem />
+                <CardItem />
+                <CardItem />
+                <CardItem />
+                <CardItem />
+                <CardItem />
+                <CardItem />
+            </div>
         </div>
     )
 }
