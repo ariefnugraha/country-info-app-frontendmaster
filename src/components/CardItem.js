11627c84a4878f02d1932691889104401cb19b22
Arief <muhammadariefnugraha77@gmail.com>
2021-08-11 20:16:33 +0700

    commit because formatter vs code

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -7,7 +7,7 @@ export default function CardItem() {
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/US_flag_51_stars.svg/1235px-US_flag_51_stars.svg.png" alt="flag" />
             </figure>
             <div className="content">
-            <p className="name">Indonesia</p>
+                <p className="name">Indonesia</p>
                 <p><span>Population:</span> 1.000.000</p>
                 <p><span>Region:</span> Oceania</p>
                 <p><span>Capital:</span> Jakarta</p>
