ab0c08262428dda48c3156c95970ff21a0ba444b
Arief <muhammadariefnugraha77@gmail.com>
2021-08-11 19:22:25 +0700

    create search and filter component for homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Navbar from '../components/Navbar';
+import SearchFilter from '../components/SearchFilter';
 
 export default function Homepage() {
     return (
-        <div className="homepage">
+        <div className="homepage day">
             <Navbar />
+            <SearchFilter />
         </div>
     )
 }
